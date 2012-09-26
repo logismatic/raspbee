fbef3276d55a23bc086d849ca10be62d4c7a2c04
Paul <paul@logismatic>
2012-09-26 16:38:23 +0200

    Comments added

--- ring.erl
+++ ring.erl
@@ -1,15 +1,27 @@
+%% ********************************************************* Module ring
+%% @copyright Logismatic GmbH 2012
+%% @author	paul.hoesli
+%% @doc		Erlang Benchmarks
+%%
+%% This erlang ring benchmark creates N processes in a ring and
+%% sends a message round the ring M times, so that a total of N * M
+%% messages get sent. 
+%%
+%% @version	0.9.1
+%% *********************************************************************
 -module(ring).
+
+%% ********************************************************* Exports ***
 -export([start/2]).
 
-%% message processing loop
+%% ***************************************************** Processing Loop
 forward(Next) ->
    Self = self(),
 	receive
-		{_Any, 0} -> %% This is the stop message, forward and die.
+		{_Any, 0} -> %% The stop message, forward and die.
 			Next ! {_Any, 0}; 
 			
-		{Self, 1} -> %% This is the last lap
-			%% send stop message
+		{Self, 1} -> %% The last lap, now send stop message
 			Next ! {Self, 0}, 
 			
 			{_, Runtime} = statistics(runtime),
@@ -28,25 +40,31 @@ forward(Next) ->
 			forward(Next)
 	end.
 	
-prc(1)  ->
-	%% prc(1) will forward to prc(NProcs) (registered as 'ring')
-	%% this closes the ring
-	forward(ring);
-prc(N)  ->
-	%% prc(N) will forward to prc(N-1)
-	forward(spawn(fun() -> prc(N-1) end)).
-		
+%% **************************************************** Process Function
+%% prc(N) will forward to prc(N-1)
+%% prc(1) will forward to the process registered as 'ring' 
+%%  this closes the ring
+prc(1) -> forward(ring);
+prc(N) -> forward(spawn(fun() -> prc(N-1) end)).
+
+%% *************************************************** API Functions ***
+%% --------------------------------------------------------- start/2 ---
+%% @spec start( nLaps(), nProcs() ) -> string().
+%% @type nProcs() = integer() 
+%% @type nLaps() = integer() 
+%%
+%% @doc Creates a ring of NProcs processes and sends initial message.
+%%
 start(NLaps,NProcs) ->
-	%% Create a ring of processes
 	register(ring, spawn_link(fun() -> prc(NProcs) end)),
 	
 	statistics(runtime),
 	statistics(wall_clock),
 	
-	%% Send a start message
+	%% Send initial message
 	ring ! {whereis(ring), NLaps + 1}, 
 
 	io:format("Runs a message in ~p laps around a ring of ~p processes.~n", 
 		[NLaps, NProcs]).
 
-
+%% ********************************************************* Module ring
